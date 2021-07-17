c86a8a16c5ff4869d5f8bad4e4fe903f2b2ab7e3
niyati1103 <75956406+niyati1103@users.noreply.github.com>
2021-07-17 19:45:26 -0400

    console.log

--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+console.log("hello")
 
 
     ground = new Ground(600,height,1200,20);
